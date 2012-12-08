5c4548bc628a425721a67cb6d29e418e81935cd1
Nicolas FRANCOIS <nicolas.franc@gmail.com>
2012-12-08 19:21:54 +0100

    chaining

--- web/client.dart
+++ web/client.dart
@@ -1,22 +1,21 @@
 library time_client;
 
 import 'dart:html';
-//#import('/home/nicolas/dart/dart-sdk/lib/i18n/date_format.dart');
 
-Element _currentTimeElement; 
+Element _currentTimeElement;
 
 main() {
   _currentTimeElement = document.query('#currentTime');
-  var source = new EventSource('/time');
-  source.on.open.add((e) => print('open'));
-  source.on.error.add((Event e) => print(e));
-  source.on.message.add((MessageEvent me)  => showDate(int.parse(me.data)));
-  
-  _currentTimeElement.text = "Start";
 }
 
-showDate(int millisecondsSinceEpoch){
+_connectToEventSource(value){
+  var source = new EventSource('/time');
+  source.on..open.add((e) => print('open'))
+           ..error.add((Event e) => print(e))
+           ..message.add((MessageEvent me)  => _displayDate(int.parse(me.data)));
+}
+
+_displayDate(int millisecondsSinceEpoch){
   Date current = new Date.fromMillisecondsSinceEpoch(millisecondsSinceEpoch);
-  //DateFormat.Hms.format(current);
   _currentTimeElement.text = current.toString();
 }
